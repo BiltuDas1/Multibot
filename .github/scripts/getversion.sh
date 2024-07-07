c516d63c832363f543457bc3226602c6a0e0f796
Biltu Das <billionto@gmail.com>
2024-07-07 21:13:57 +0530

    Changed variable name to Version and VersionType

--- .github/scripts/getversion.sh
+++ .github/scripts/getversion.sh
@@ -3,5 +3,5 @@ Ver=$(cat version)
 Version=$(echo $Ver | cut -f1 -d-)
 VersionType=$(echo $Ver | cut -f2 -d-)
 
-echo "VERSION=$Version" >> $GITHUB_ENV
-echo "VERSIONTYPE=$VersionType" >> $GITHUB_ENV
+echo "Version=$Version" >> $GITHUB_ENV
+echo "VersionType=$VersionType" >> $GITHUB_ENV
